318a255ab686552f17954628a29a954be625e9bf
Ariel Diamond <ariel.diamond@gmail.com>
2014-06-12 10:01:11 -0500

    model associations 1st draft

--- app/models/activity_participation.rb
+++ app/models/activity_participation.rb
@@ -1,2 +1,4 @@
 class ActivityParticipation < ActiveRecord::Base
+	belongs_to :participant, class_name: "User"
+	belongs_to :activity
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ActiveRecord::Base
+	#as a trip-goer
+	has_many :trip_participations
+	has_many :trips, through: :trip_participations, foreign_key: :traveler_id
+
+	#as a creator of an activity
+	has_many :created_activities, source: :activity, foreign_key: :creator_id
+
+	#as a creator of a trip
+	has_many :created_trips, source: :trip, foreign_key: :creator_id
 end
