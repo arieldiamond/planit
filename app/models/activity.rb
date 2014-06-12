2acb6630e9b96f04e5ee160cf077f27486508dfd
Ariel Diamond <ariel.diamond@gmail.com>
2014-06-12 10:18:10 -0500

    notes

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,8 +1,12 @@
 class Activity < ActiveRecord::Base
-	#
+	#activity-goers
 	has_many :activity_participations
 	has_many :participants, class_name: "User", through: :activity_participations
+
+	# association with trip
 	has_many :trip_participations, through: :activity_participations
-	belongs_to :creator, class_name: "User"
 	belongs_to :trip
+
+	# belongs to trip creator
+	belongs_to :creator, class_name: "User"
 end
