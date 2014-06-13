251f774d518d30ee4b56428ceca88b5f1a3d8ea4
Ariel Diamond <ariel.diamond@gmail.com>
2014-06-12 21:18:48 -0500

    can edit trips

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,9 +1,39 @@
 class TripsController < ApplicationController
+	def new
+	end
+
+	def create
+	end
+
 	def index
 		@trips = Trip.all
 	end
 	
 	def show
 		@trip = Trip.find(params[:id])
+	end
+
+	def edit
+		@trip = Trip.find(params[:id])
+	end
+
+	def update
+		@trip = Trip.find(params[:id])
+		if @trip.update_attributes(trip_params)
+			redirect_to trip_path
+		else
+			render :action => "edit"
+		end
+	end
+
+
+	def destroy
+		@trip = Trip.find(params[:id])
+		@trip.destroy
+	end
+
+	private
+	def trip_params
+		params.require(:trip).permit(:name, :description, :location, :start_date, :end_date)
 	end
 end
