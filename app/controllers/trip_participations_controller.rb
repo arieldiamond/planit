7d0308f6387385debe81df5694d448fc27671302
Anne Baer <albaer@gmail.com>
2014-06-15 14:09:09 -0500

    most of in-app invites working

--- app/controllers/trip_participations_controller.rb
+++ app/controllers/trip_participations_controller.rb
@@ -62,6 +62,8 @@ class TripParticipationsController < ApplicationController
   end
 
   private
+
+
     # Use callbacks to share common setup or constraints between actions.
     def set_trip_participation
       @trip_participation = TripParticipation.find(params[:id])
