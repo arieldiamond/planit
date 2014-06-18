3640e5b55abe4dfa3a54add998efdeeebb168a86
fab9 <rorou192@gmail.com>
2014-06-18 17:59:54 -0500

    add welcome layout

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,10 @@
 class WelcomeController < ApplicationController
-  layout 'application'
+  # layout 'application'
+  layout 'welcome'
 
   def index
-  	if user_signed_in?
-  		redirect_to trips_path
-  	end
+      if user_signed_in?
+      	redirect_to trips_path
+      end
   end
 end
