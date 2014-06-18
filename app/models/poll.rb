d5071eeeb8683f178de08fa0afd9d6a0f212a135
Jaimin Patel <jcpatel@gmail.com>
2014-06-17 19:09:07 -0500

    added most upvotes

--- app/models/poll.rb
+++ app/models/poll.rb
@@ -3,4 +3,16 @@ class Poll < ActiveRecord::Base
 	belongs_to :creator, class_name: "User"
 	has_many :options, dependent: :destroy
 	accepts_nested_attributes_for :options, :reject_if => :all_blank, :allow_destroy => true
+
+	def leader_name
+		self.leader.name
+	end
+
+	def leader
+		self.options.max_by do |option|
+			option.upvotes
+		end
+	end
+
+	
 end
