294421cd8d8c00f31fad07b98e80284b10ba0926
Dusty Wilson <dwwilson21@gmail.com>
2014-06-19 13:03:00 -0500

    fixed misnamed params coming from mobile

--- app/controllers/mobile_activities_controller.rb
+++ app/controllers/mobile_activities_controller.rb
@@ -11,10 +11,10 @@ class MobileActivitiesController < ApplicationController
 			location: params[:location], 
 			start_time: params[:start_time], 
 			end_time: params[:end_time],
-			trip_id: params[:trip_id],
+			trip_name: params[:trip_name],
 			creator_id: params[:creator_id] 
 			)
-		@trip = Trip.find(params[:trip_id])
+		@trip = Trip.find_by_trip_name(params[:trip_name])
     @user = User.find(params[:creator_id])
     @trip.activities << @activity
 
